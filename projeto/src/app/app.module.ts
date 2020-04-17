18b38c0a276fb07e7109bfaf7bf7692ab51bb82e
nanduh007 <luiz_paiva@gec.inatel.br>
2020-04-17 15:01:46 -0300

    guardando alterações

--- projeto/src/app/app.module.ts
+++ projeto/src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { UserRegisterComponent } from './user/user-register.component';
+import { HomeComponent } from './home/app-home.component';
+import { LoginComponent } from './login/app-login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserRegisterComponent
+    UserRegisterComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: '', redirectTo: 'login', pathMatch: 'full'
+      },
+      {
+        path: 'login', component: LoginComponent
+      },
+      {
+        path: 'home', component: HomeComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ projeto/src/app/home/app-home.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    // selector: 'app-home', // não precisa mais do selector pois o está sendo linkado por rota e não pelo selector
+    templateUrl: './app-home.component.html'
+})
+export class HomeComponent{
+
+}
